ac0ac6996d2bed6287a357d9f4f30803d88419f2
DeveloperRaghavan <developer.raghavan@gmail.com>
2022-09-18 18:15:35 +0530

    All the File Upload to Github

--- Admin/javascript/firebaseIndex.js
+++ Admin/javascript/firebaseIndex.js
@@ -4,7 +4,7 @@ document.getElementById("loginForm").addEventListener("submit",(event)=>{
 
 firebase.auth().onAuthStateChanged((user)=>{
     if(user){
-        location.replace("dashboard.html")
+        location.replace("../Angular/q3carcare/src/index.html")
     }
 })
 
